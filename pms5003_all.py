c4ea92465c6a2152598ee0e3fbcbdd7be2bd3ada
Katie-Shay <63169265+Katie-Shay@users.noreply.github.com>
2025-07-31 22:47:07 -0400

    Update pms5003_all.py

--- pms5003_all.py
+++ pms5003_all.py
@@ -31,12 +31,12 @@ with open(data_file, "a") as file:
                "# * PM 1.0 in μg/m³ atmospheric environment\n" \
                "# * PM 2.5 in μg/m³ atmospheric environment\n" \
                "# * PM 10 in μg/m³ atmospheric environment\n" \
-               "# * number of particles >0.3 μm / 0.1 dm³ of air\n" \
-               "# * number of particles >0.5 μm / 0.1 dm³ of air\n" \
-               "# * number of particles >1.0 μm / 0.1 dm³ of air\n" \
-               "# * number of particles >2.5 μm / 0.1 dm³ of air\n" \
-               "# * number of particles >5 μm / 0.1 dm³ of air\n" \
-               "# * number of particles >10 μm / 0.1 dm³ of air\n".format(datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")))
+               "# * number of particles >0.3 μm / 0.1 L of air\n" \
+               "# * number of particles >0.5 μm / 0.1 L of air\n" \
+               "# * number of particles >1.0 μm / 0.1 L of air\n" \
+               "# * number of particles >2.5 μm / 0.1 L of air\n" \
+               "# * number of particles >5 μm / 0.1 L of air\n" \
+               "# * number of particles >10 μm / 0.1 L of air\n".format(datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")))
 
 
 while True:
@@ -73,12 +73,12 @@ while True:
     pgw_pms5003.labels('PM 2.5 in μg/m³ atmospheric environment').set(sensor_raw_data.pm_ug_per_m3(2.5, atmospheric_environment=True))
     pgw_pms5003.labels('PM 10 in μg/m³ atmospheric environment').set(sensor_raw_data.pm_ug_per_m3(None,atmospheric_environment=True))
 
-    pgw_pms5003.labels('number of particles >0.3 μm / 0.1 dm³ of air').set(sensor_raw_data.pm_per_1l_air(0.3))
-    pgw_pms5003.labels('number of particles >0.5 μm / 0.1 dm³ of air').set(sensor_raw_data.pm_per_1l_air(0.5))
-    pgw_pms5003.labels('number of particles >1.0 μm / 0.1 dm³ of air').set(sensor_raw_data.pm_per_1l_air(1.0))
-    pgw_pms5003.labels('number of particles >2.5 μm / 0.1 dm³ of air').set(sensor_raw_data.pm_per_1l_air(2.5))
-    pgw_pms5003.labels('number of particles >5 μm / 0.1 dm³ of air').set(sensor_raw_data.pm_per_1l_air(5))
-    pgw_pms5003.labels('number of particles >10 μm / 0.1 dm³ of air').set(sensor_raw_data.pm_per_1l_air(10))
+    pgw_pms5003.labels('number of particles >0.3 μm / 0.1 L of air').set(sensor_raw_data.pm_per_1l_air(0.3))
+    pgw_pms5003.labels('number of particles >0.5 μm / 0.1 L of air').set(sensor_raw_data.pm_per_1l_air(0.5))
+    pgw_pms5003.labels('number of particles >1.0 μm / 0.1 L of air').set(sensor_raw_data.pm_per_1l_air(1.0))
+    pgw_pms5003.labels('number of particles >2.5 μm / 0.1 L of air').set(sensor_raw_data.pm_per_1l_air(2.5))
+    pgw_pms5003.labels('number of particles >5 μm / 0.1 L of air').set(sensor_raw_data.pm_per_1l_air(5))
+    pgw_pms5003.labels('number of particles >10 μm / 0.1 L of air').set(sensor_raw_data.pm_per_1l_air(10))
 
     push_to_gateway(push_gateway_url, job=job_name, registry=registry, grouping_key={'instance': instance_name})
 
